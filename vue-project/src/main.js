07ab72a86f58cbd2a49b198b57147ddba266a8fd
Diba <88105499+dee-ba@users.noreply.github.com>
2022-10-27 20:02:32 +0200

    vue

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
 import './assets/main.css'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:8000/api/';
 
 const app = createApp(App)
 
 app.use(router)
 
 app.mount('#app')
+
