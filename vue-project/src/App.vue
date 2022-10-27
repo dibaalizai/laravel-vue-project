668e58107257df74be578bee51923f77db92fe9d
Diba <88105499+dee-ba@users.noreply.github.com>
2022-10-27 21:40:00 +0200

    commit

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -5,20 +5,15 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
 
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
 
-  
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Laravel-Vue-Project" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">Users</RouterLink>
+        <RouterLink to="/about">Products</RouterLink>
       </nav>
     </div>
   </header>
